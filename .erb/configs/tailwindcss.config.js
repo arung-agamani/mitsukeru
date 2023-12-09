540e0e4d04c4350d4a668cff970e6681c4b007f4
Arung Agamani <arung.agamani@gmail.com>
2023-12-10 07:05:36 +0800

    chore: add tailwind

---
+++ .erb/configs/tailwindcss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: ['./src/**/*.{tsx,jsx,js,ts}'],
+};
